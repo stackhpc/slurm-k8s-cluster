a1bd3706ed8fadd952d44fd15cee5c3651f04a77
Will <iq20064@bristol.ac.uk>
2023-07-17 13:51:29 +0100

    Trying creating shell directory on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -107,6 +107,7 @@ then
     /usr/libexec/httpd-ssl-gencerts
     /opt/ood/ood-portal-generator/sbin/update_ood_portal
     mkdir --parents /opt/rh/httpd24/root/etc/httpd/
+    mkdir --parents /etc/ood/config/apps/shell
     /usr/bin/htdbm -cb /opt/rh/httpd24/root/etc/httpd/.htpasswd.dbm rocky password
     /usr/sbin/httpd -k start -X -e debug
 fi
