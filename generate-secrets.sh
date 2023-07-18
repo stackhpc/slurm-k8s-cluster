ad79e16d07774696417f437ba1a16e6959cbf4a0
Will <iq20064@bristol.ac.uk>
2023-07-18 15:30:05 +0100

    Rocky OOD password now set as secret from generate-secrets.sh

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -11,3 +11,16 @@ kubectl create secret generic munge-key-secret \
 --from-literal=munge.key=$(dd if=/dev/urandom bs=1 count=1024 2>/dev/null | base64 -w 0) \
 -o yaml | \
 kubectl apply -f -
+
+OOD_PASS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 16)
+
+kubectl create secret generic htdbm-secret \
+--dry-run=client \
+--from-literal=password=$OOD_PASS \
+-o yaml | \
+kubectl apply -f -
+
+echo "Open Ondemand Credentials:"
+echo "Username: rocky"
+echo "Password: $OOD_PASS"
+OOD_PASS=""
