3f86fbe7853e977eb8099e00b2f52e99a271235a
Will <iq20064@bristol.ac.uk>
2023-07-18 14:00:53 +0100

    Trying entrypoint tweaks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -104,11 +104,14 @@ then
     echo "---> MUNGE Complete"
 
     echo "---> Starting Apache Server"
+
+    mkdir --parents /etc/ood/config/apps/shell
+    env > /etc/ood/config/apps/shell/env
+
     /usr/libexec/httpd-ssl-gencerts
     /opt/ood/ood-portal-generator/sbin/update_ood_portal
     mkdir --parents /opt/rh/httpd24/root/etc/httpd/
-    mkdir --parents /etc/ood/config/apps/shell
-    touch /etc/ood/config/apps/shell/env
+
     /usr/bin/htdbm -cb /opt/rh/httpd24/root/etc/httpd/.htpasswd.dbm rocky password
     /usr/sbin/httpd -k start -X -e debug
 fi
