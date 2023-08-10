a9003f7a17aba22cc1c0e373f8fbfa6e73c6f742
Will <iq20064@bristol.ac.uk>
2023-08-10 13:37:40 +0100

    Moved OOD password to values/yaml

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -8,16 +8,3 @@ kubectl create secret generic host-keys-secret \
 -o yaml | \
 kubectl apply -f -
 rm -rf ./temphostkeys
-
-OOD_PASS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 16)
-
-kubectl create secret generic htdbm-secret \
---dry-run=client \
---from-literal=password=$OOD_PASS \
--o yaml | \
-kubectl apply -f -
-
-echo "Open Ondemand Credentials:"
-echo "Username: rocky"
-echo "Password: $OOD_PASS"
-OOD_PASS=""
