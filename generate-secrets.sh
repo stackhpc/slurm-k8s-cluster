f86952f405ee251f212024a58d8ec6dd75e40314
Scott Davidson <scott@stackhpc.com>
2023-08-15 12:22:21 +0100

    Separate Rook cleanup into correct chart

--- generate-secrets.sh
+++ generate-secrets.sh
@@ -6,12 +6,12 @@ fi
 
 kubectl -n $NAMESPACE create secret generic database-auth-secret \
 --dry-run=client \
---from-literal=password=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 32) \
+--from-literal=password=abcdefghijklmnopqrstuvwxyz123456 \
 -o yaml | \
 kubectl -n $NAMESPACE apply -f -
 
 kubectl -n $NAMESPACE create secret generic munge-key-secret \
 --dry-run=client \
---from-literal=munge.key=$(dd if=/dev/urandom bs=1 count=1024 2>/dev/null | base64 -w 0) \
+--from-literal=munge.key=$(dd if=/dev/urandom bs=1 count=1024 2>/dev/null | base64) \
 -o yaml | \
 kubectl -n $NAMESPACE apply -f -
